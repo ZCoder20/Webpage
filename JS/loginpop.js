4e34ff98801463c5930977af0581d7970abb797c
Bhavnish <75770979+ZCoder20@users.noreply.github.com>
2023-08-13 10:08:18 -0500

    Working copy

--- JS/loginpop.js
+++ JS/loginpop.js
@@ -10,3 +10,38 @@ function closepopup()
   window.close();
  // document.getElementById('popup-overlay').style.display = 'none';
 }
+
+
+function loginclick()
+{
+
+  username = document.getElementById('username');
+  password = document.getElementById('password');
+  fetch(`http://localhost:8080/vi/api/getlogin?username=${username.value}&password=${password.value}`)
+      .then(response => response.json())
+      .then(data => {
+        // Access the specific element from the JSON response
+
+        data.forEach(element => {
+          // Access specific properties of each array element
+
+          const myObj = JSON.parse(JSON.stringify(element));
+
+            localStorage.setItem('userid', myObj.userid);
+            localStorage.setItem('userlogin', true);
+            localStorage.setItem('username', myObj.username);
+
+          //  window.open('index.html','self');
+            window.location.replace('index.html');
+            alert("");
+        });
+
+
+
+
+      })
+      .catch(error => {
+        console.error('Error calling API:', error);
+      });
+
+}
